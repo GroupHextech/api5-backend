bd29929e3b3cfdff56df684c81d70eb9e5feaa38
Augusto <augustotsantos@outlook.com>
2023-11-04 20:43:01 -0300

    add crossOrigin

--- cloudKitchen/src/main/java/api5/cloudKitchen/controller/EstoqueController.java
+++ cloudKitchen/src/main/java/api5/cloudKitchen/controller/EstoqueController.java
@@ -1,5 +1,6 @@
 package api5.cloudKitchen.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import api5.cloudKitchen.service.EstoqueService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/estoque")
 public class EstoqueController {
 
